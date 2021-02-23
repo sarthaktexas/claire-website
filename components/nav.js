d8b9c06b1da40f0eae863750bcf6bb3e89a7f9d2
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-02-23 04:10:47 -0600

    🚑 fix nav bar

--- components/nav.js
+++ components/nav.js
@@ -5,15 +5,14 @@ const links = [
     { label: "Scrapbook", href: "https://scrapbook.hackclub.com/clairebookworm/" },
     { label: "Gallery", href: "/gallery" },
     { label: "Writing", href: "/writing" },
-
 ]
 
 export default function Nav() {
     return (
         <nav className="text-white">
-            <ul className="flex flex-wrap sm:justify-between items-start sm:items-center p-8">
-                <li className="hidden sm:block"></li>
-                <ul className={`mx-auto sm:mx-0 grid gap-4 grid-cols-${links.length > 4 ? '3' : '2'} sm:grid-rows-1 sm:grid-cols-${links.length} mt-6 sm:mt-0`}>
+            <ul className="flex flex-wrap sm:justify-between items-start sm:items-center p-8 mt-6 sm:mt-0">
+                <li className="hidden sm:block sm:grid-cols-1 sm:grid-cols-2 sm:grid-cols-3 sm:grid-cols-4 sm:grid-cols-5 sm:grid-cols-6 sm:grid-cols-7 sm:grid-cols-8 sm:grid-cols-9 sm:grid-cols-10 sm:grid-cols-11 sm:grid-cols-12"></li>
+                <ul className={`mx-auto sm:mx-0 grid gap-4 ${links.length > 4 ? 'grid-cols-3' : 'grid-cols-2'} sm:grid-rows-1 sm:grid-cols-${links.length}`}>
                     {links.map(({ href, label }) => (
                         <li key={`${href}${label}`}>
                             <Link href={href}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,20 @@
+const num = Array.from({length: 12}, (_, i) => i + 1)
+const whitelist = []
+num.map((x) => {
+  whitelist.push('translate-x-' + x)
+  whitelist.push('-translate-x-' + x)
+  whitelist.push('translate-y-' + x)
+  whitelist.push('-translate-y-' + x)
+})
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    options: {
+      whitelist,
+    },
+  },
   darkMode: 'class',
   theme: {
     extend: {
